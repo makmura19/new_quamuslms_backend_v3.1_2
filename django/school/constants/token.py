acf8b2a52e36f39e3af693e715569ab8b9bc0891
Irvan Gumilar <irvangumilar87@gmail.com>
2025-08-25 12:35:00 +0700

    update

--- django/school/constants/token.py
+++ django/school/constants/token.py
@@ -5,4 +5,4 @@ class TOKEN_NAME(str, Enum):
     ACCESS_TOKEN = "1LnWWq78d3UJ4toxoBwugYe8B79YH9P5iAvxzxZx29wT9hNE1I"
     REFRESH_TOKEN = "imGTt093dqMiM11y28rnp1tt8Ujm7N9c9FtU532Jjr6Ie5wXyA"
     USER_INFO = "36RflA0Yu861WlDn2g98r2ZtI9z193xf3Pp97L4v1fVGN1Wt06"
-    COOKIE_DOMAIN = ".quamuslms.id"
+    COOKIE_DOMAIN = ".stagingquamuslms.com"
